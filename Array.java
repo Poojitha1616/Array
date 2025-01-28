32d6d75cff29ee1eb58c9611c63e9f98c5b8d030
Poojitha1616 <gudemsaipoojitha2@gmail.com>
2025-01-28 23:18:35 +0530

    Add files via upload
    this program displays and print elements of a 1D array

---
+++ Array.java
@@ -0,0 +1,12 @@
+public class Array {
+    public static void main(String[] args) {
+        int size = 5;
+        int[] array = {10, 20, 30, 40, 50};
+
+        System.out.println("Elements of the array:");
+        for (int i = 0; i < size; i++) {
+            System.out.print(array[i] + " ");
+        }
+    }
+}
+
